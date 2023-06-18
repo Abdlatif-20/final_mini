0f793b77558db700edaa2b710970d9c9af7820c8
ABDELLATYF EN-NEIYMY <rca8272@gmail.com>
2023-06-18 19:43:27 +0100

    fix leak with unset pdw and oldpwd

--- execution/built_in_functions/my_unset.c
+++ execution/built_in_functions/my_unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   my_unset.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahaloui <ahaloui@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aben-nei <aben-nei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 17:24:38 by ahaloui           #+#    #+#             */
-/*   Updated: 2023/06/13 21:15:32 by ahaloui          ###   ########.fr       */
+/*   Updated: 2023/06/18 19:31:15 by aben-nei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,14 @@ void	my_unset(t_cmd *cmd, t_info *info)
 	while (cmd->cmds[i])
 	{
 		variable = get_env_variable(cmd->cmds[i]);
+		if (check_if_valid_args(cmd->cmds[i]) == 0)
+		{
+			ft_putstr_fd("minishell: unset: `", 2);
+			ft_putstr_fd(cmd->cmds[i], 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+			i++;
+			continue ;
+		}
 		remove_env_element(&info->head_en, variable);
 		remove_export_element(&info->head_ex, variable);
 		free(variable);
